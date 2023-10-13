a79e9ea2fea8b089612f66a065c14d36e20ea6a7
giankpetrov <>
2023-10-13 23:57:14 +0000

    ADD allauth templates and base.html

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 # required allauth
-                'django.template.context_processors.request',  
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
