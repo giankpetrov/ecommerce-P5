6b0ed7951bf79f45970bd1d35180fb63dd3cf84a
giankpetrov <>
2023-10-24 21:48:54 +0000

    FIX allowed host when moved teams on codeanywhere

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = [
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-giankpetrov-ecommerce-p5-d44g7eycsn.us2.codeanyapp.com']
+ALLOWED_HOSTS = [
+    '8000-giankpetrov-ecommerce-p5-vxh9o5aqwc.us2.codeanyapp.com']
 
 
 # Application definition
