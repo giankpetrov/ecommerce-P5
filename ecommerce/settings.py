b3dea2a80d1cb4afa93818f789ba4523535c2897
giankpetrov <>
2023-10-26 10:55:34 +0000

    removed message import settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
-from django.contrib.messages import constants as messages
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,7 +30,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-giankpetrov-ecommerce-p5-vxh9o5aqwc.us2.codeanyapp.com',
                  'gianncarlociampaglia-5p-5865812be146.herokuapp.com',
-                 
+
                  'localhost',
                  'gianncarlociampaglia-5pherokuapp.com']
 
