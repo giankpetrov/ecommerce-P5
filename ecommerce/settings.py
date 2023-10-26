0a077bb4ec1af24cad259425cc0a229229cc6876
giankpetrov <>
2023-10-26 10:44:46 +0000

    removed messages import settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
-from django.contrib.messages import constants as messages
+
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
