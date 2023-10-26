1bcebe207692d4e18266c4fffaa40e8a04bef0a8
giankpetrov <>
2023-10-26 21:10:16 +0000

    ADD cache control

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -189,6 +189,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Buckert Config
     AWS_STORAGE_BUCKET_NAME = "gianncarlociampaglia-5p"
     AWS_S3_REGION_NAME = "eu-west-1"
